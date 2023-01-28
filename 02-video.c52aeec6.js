0cd81b01f3a41a79395002e62d3ad392069824a9
HronaOlha <111655363+HronaOlha@users.noreply.github.com>
2023-01-28 23:02:18 +0000

    Deploying to gh-pages from @ HronaOlha/goit-js-hw-08@0852f3272e9158e6e10bc73194d11d9f813d0c20 🚀

---
+++ 02-video.c52aeec6.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("vimeo-player"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)}))}();
+//# sourceMappingURL=02-video.c52aeec6.js.map
